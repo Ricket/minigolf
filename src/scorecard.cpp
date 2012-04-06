ef2185172e8084dd134a8160bca637ac1b1a6c10
Richard Carter <richard@richardcarter.org>
2012-04-06 18:47:36 -0400

    Add scorecard file skeleton

---
+++ src/scorecard.cpp
@@ -0,0 +1,5 @@
+#include "scorecard.h"
+
+GLUI *create_scorecard(struct player* players[4]) {
+	return NULL;
+}
---
+++ src/scorecard.h
@@ -0,0 +1,8 @@
+#ifndef SCORECARD_H
+#define SCORECARD_H
+
+#include <GL/glui.h>
+
+GLUI *create_scorecard(struct player**);
+
+#endif
